800df38db4be09ea2da819e6638350637b4f6748
jpurvis00 <46824604+jpurvis00@users.noreply.github.com>
2019-11-11 14:25:19 -0500

    Update JdbcTest.java

--- JdbcTest.java
+++ JdbcTest.java
@@ -24,11 +24,74 @@ public class JdbcTest {
             while(myRs.next()){
                 System.out.println(myRs.getString("last_name") + ", " + myRs.getString("first_name"));
             }
+
+            displayEmployee(myStmt, "Eric", "Wright");
+
+            //Insert data into db
+            int rowsAffected = myStmt.executeUpdate("insert into employees (last_name, first_name, email, department, salary) values ('Wright', 'Eric', 'eric.wright@foo.com', 'HR', 33000.00)");
+
+            displayEmployee(myStmt, "Eric", "Wright");
+
+            //Update data in db
+            int rowsAffected2 = myStmt.executeUpdate("update employees set email='eric.wright@foobar.com' where last_name='Wright' and first_name='Eric'");
+
+            displayEmployee(myStmt, "Eric", "Wright");
+
+            //Delete info from the db
+            int rowsAffected3 = myStmt.executeUpdate("delete from employees where last_name='Wright' and first_name='Eric'");
+
+            displayEmployee(myStmt, "Eric", "Wright");
+
+            displaySalary(myConn);
         } catch (Exception e){
             e.printStackTrace();
+        } finally {
+            if(myRs != null)
+                myRs.close();
         }
     }
+
+    /*Returns the results of the employee we are querying*/
+    public static void displayEmployee(Statement stmt, String firstName, String lastName) throws SQLException{
+        boolean found = false;
+        ResultSet results = stmt.executeQuery("select * from employees where last_name='" + lastName + "' and first_name='" + firstName + "'");
+
+        /*Cycle through all results returned and print first name, last name and email*/
+        while (results.next()) {
+            System.out.println(results.getString("first_name") + ", " + results.getString("last_name") + " - " + results.getString("email"));
+            found = true;
+        }
+
+        /*No results were found*/
+        if(!found)
+            System.out.println("Employee not found");
+    }
+
+    /*Method is using prepared statement instead of hard coding sql statements.  ? takes the place of hard coded
+    * values.  */
+    public static void displaySalary(Connection myConn) throws SQLException{
+        boolean found = false;
+        PreparedStatement myStmt = myConn.prepareStatement("select * from employees where salary > ? and department=?");
+
+        /*Replaces first ? in above statement*/
+        myStmt.setDouble(1, 80000);
+        /*Replaces second ? in above statement*/
+        myStmt.setString(2, "Legal");
+
+        /*Execute the prepared statement*/
+        ResultSet results = myStmt.executeQuery();
+
+        while(results.next()){
+            System.out.println(results.getString("first_name") + ", " + results.getString("last_name") + " - " + results.getString("salary") + ", " + results.getString("department"));
+            found = true;
+        }
+
+        /*No results were found*/
+        if(!found)
+            System.out.println("No matching data");
+    }
 }
+
 
 /* In order for this to work, we had to add the mysql connector .jar file to our project.  This was done 
 * by downloading the jar file and then adding it to our project.  Following steps are for intellij
